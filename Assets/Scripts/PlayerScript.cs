85ea4ae003b1c7c87abcb915c416ee0f97c1d4f6
daistic <matotsuper@gmail.com>
2024-06-03 23:40:56 +0700

    Camera Control
    :(

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public float moveSpeed = 5f;
-    
+    public Rigidbody rb;
+    public GameObject viewCam;
+    public float moveSpeed = 5;
+    public float mouseSensitivity = 10;
 
     void Start()
     {
@@ -15,8 +16,16 @@ public class PlayerScript : MonoBehaviour
 
     void Update()
     {
-        Vector3 moveForward = transform.up * Input.GetAxis("Vertical");
-        Vector3 moveSideways = transform.right * Input.GetAxis("Horizontal");
+        //player movement control
+        Vector3 moveForward = transform.right * Input.GetAxis("Vertical");
+        Vector3 moveSideways = -(transform.right) * Input.GetAxis("Horizontal");
         rb.velocity = (moveForward + moveSideways) * moveSpeed;
+
+        //player view control
+        float rotateSideways = Input.GetAxisRaw("Mouse X") * mouseSensitivity;
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0, 0, rotateSideways));
+
+        float rotateVertical = Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
+        viewCam.transform.localRotation = Quaternion.Euler(viewCam.transform.localRotation.eulerAngles + new Vector3(0, rotateVertical, 0));
     }
 }
